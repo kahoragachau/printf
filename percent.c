d89ecc4cede315d3beb3aac52c4876f970dca9e3
kahoragachau <joelkahoragachau6@gmail.com>
2022-04-21 19:04:43 +0300

    - Create percent.c

---
+++ percent.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * percentage - function to check out for percentage in a program
+ * @valist: integer to be inputted
+ *
+ * Return: percentage
+ */
+
+int percentage(__attribute__((unused))va_list valist)
+{
+	_putchar('%');
+	return (1);
+}
